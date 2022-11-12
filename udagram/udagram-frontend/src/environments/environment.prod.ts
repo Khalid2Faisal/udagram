5b07d4b0f5a1e7ee4f903ad2c326bc8d636b6545
Khalid Dashash <khalid.faisal.7070@gmail.com>
2022-11-12 10:16:32 +0000

    solved linting issues

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost:
-    "http://udagram-api-dev.eba-npq7nmw9.us-east-1.elasticbeanstalk.com/api/v0",
+    'http://udagram-api-dev.eba-npq7nmw9.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
